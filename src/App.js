14a4fbbde126a548f2d54f63afcc0756e852760f
Yordan <yordan_yordanov001@abv.bg>
2022-01-10 17:35:53 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import { motion } from "framer-motion";
 
 function App() {
+  function template({ rotate, x }) {
+    return `rotate(${rotate}) translateX(${x})`;
+  }
   return (
     <div className="App">
       <motion.form
-        transformTemplate={({ x, rotate }) =>
-          `rotate(${rotate}deg) translateX(${x}px)`
-        }
+        transformTemplate={template}
+        animate={{ rotate: 360 }}
+        style={{ rotate: 0, x: "calc(50vh - 100px)" }}
       >
         <h1>Create An Account</h1>
         <label>
